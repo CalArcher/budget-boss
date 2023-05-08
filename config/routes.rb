13ab2626127a7bcae18dc8b5b7ede570ce90962c
Cal Archer <p.calarch@gmail.com>
2023-05-07 20:00:11 -0400

    add healthcheck route for render

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :text_recieves
   resources :transactions 
 
+  get "/healthcheck", to: proc { [200, {}, ["OK"]] }
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
