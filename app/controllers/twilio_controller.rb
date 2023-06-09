143844e6bc679ceaa1a8663b2059e2432fe7667a
Cal Archer <p.calarch@gmail.com>
2023-06-08 21:50:40 -0400

    rm unnecessary routes and add twilio route

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,11 @@
+class TwilioController < ApplicationController
+  protect_from_forgery except: :receive_text
+
+  # TODO
+  # def receive_text
+  #   body = params["Body"]
+  #   sender_number = params["From"]
+  #   IncomingSmsService.new(body: body, from_number: sender_number).handle_reply
+  #   head :ok
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :bills
   resources :sheets
-  resources :text_sends
-  resources :text_recieves
   resources :transactions 
 
+  # post '/receive_text', to: 'twilio#receive_text'
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
