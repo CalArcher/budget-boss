2314c96fba6d43f337d31fbba141e75822a3c312
Cal Archer <p.calarch@gmail.com>
2023-06-12 20:06:41 -0400

    update to find number from user, not ENV

--- app/services/outgoing_sms_service.rb
+++ app/services/outgoing_sms_service.rb
@@ -1,16 +1,18 @@
 class OutgoingSmsService
-  def initialize(to_user_id:, body:)
-    @to_user_id = to_user_id
+  def initialize(to_user_key:, body:)
+    @to_user_key = to_user_key
     @body = body
   end
 
+  def to_user
+    ::User.find_by!(key: to_user_key)
+  end
 
   def send
     client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
 
-
     from = ENV['TWILIO_NUMBER']
-    to = ENV[@to_user_id.to_s]
+    to = to_user.phone_number
     puts "sent to #{to}: #{@body}"
     # client.messages.create(
       # from: from,
