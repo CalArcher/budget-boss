5fbd7e1e00ddc547d108539956402d6c454856fc
Cal Archer <p.calarch@gmail.com>
2023-06-12 21:40:51 -0400

    pass in user instead of user key

--- app/services/outgoing_sms_service.rb
+++ app/services/outgoing_sms_service.rb
@@ -1,18 +1,14 @@
 class OutgoingSmsService
-  def initialize(to_user_key:, body:)
-    @to_user_key = to_user_key
+  def initialize(to_user:, body:)
+    @to_user = to_user
     @body = body
-  end
-
-  def to_user
-    ::User.find_by!(key: to_user_key)
   end
 
   def send
     client = Twilio::REST::Client.new(ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN'])
 
     from = ENV['TWILIO_NUMBER']
-    to = to_user.phone_number
+    to = @to_user.phone_number
     puts "sent to #{to}: #{@body}"
     # client.messages.create(
       # from: from,
