e4a7a7a69e590242716453548092afde6914c8dd
Cal Archer <p.calarch@gmail.com>
2023-07-04 16:01:17 -0400

    remove testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :bills
-  resources :sheets
-  resources :transactions 
+  # resources :bills
+  # resources :sheets
+  # resources :transactions 
 
   post '/receive_text', to: 'twilio#receive_text'
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
-  get "*path", to: "catch_all#catch_all"
+  # get "*path", to: "catch_all#catch_all"
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
