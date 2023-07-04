7d936ad9193880a71052250eec7b342f8da3d58b
Cal Archer <p.calarch@gmail.com>
2023-07-04 16:31:48 -0400

    actually validate correctly

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,9 +1,5 @@
 class TwilioController < ApplicationController
 
-  # skip_before_action :verify_authenticity_token
-
-  before_action :authenticate_twilio_request, only: [:receive_text]
-
   def receive_text
     logger.info params
     puts params
@@ -13,20 +9,5 @@ class TwilioController < ApplicationController
     sender_number = params["From"]
     IncomingSmsService.new(body: body, from_number: sender_number).process_incoming_message
     head :ok
-  end
-
-  def authenticate_twilio_request
-    puts(request.headers)
-    puts('----------------')
-    puts(request)
-    twilio_signature = request.headers['X-Twilio-Signature']
-    validator = Twilio::Security::RequestValidator.new(ENV['TWILIO_AUTH_TOKEN'])
-
-    url = url_for(only_path: false, action: 'receive_text')
-
-    unless validator.validate(url, request.parameters, twilio_signature)
-      head :unauthorized
-      return
-    end
   end
 end
