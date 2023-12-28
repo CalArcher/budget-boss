be59c8fea071c1dcb3a7ec3a91da9c697c5003a9
Cal Archer <p.calarch@gmail.com>
2023-12-27 17:45:43 -0700

    add all user status command

--- app/services/commands/user_status_command.rb
+++ app/services/commands/user_status_command.rb
@@ -29,11 +29,15 @@ module Commands
     def queried_user
       ::User.find_by(name: user_name_from_command)
     end
+    
+    def all_users?
+      split_command.first == 'all'
+    end
 
     def valid_command?
       [
         correct_length?,
-        queried_user.present?,
+        (queried_user.present? || all_users?),
         status_request?,
       ].all?
     end
@@ -51,10 +55,41 @@ module Commands
     end
 
     def execute
-      get_status(queried_user)
+      if all_users?
+        get_all_user_status
+      else
+        get_status(queried_user)
+      end
     end
 
     private
+
+    def get_all_user_status
+      sheet = ::Sheet.find_or_create_sheet(current_month, current_year)
+
+      if sheet.nil?
+        error_message = "Cannot find sheet, please try again"
+        send_message(@to_user, error_message)
+        return
+      end
+
+      reply_message = ['**So far this month:**']
+
+      ::User.all.each do |user|
+        user_budget_column = "#{user.key}_budget"
+        user_spent_column = "#{user.key}_spent"
+
+        spent_value = sheet[user_spent_column].to_i
+        remaining_value = sheet[user_budget_column].to_i
+
+        spent_message = "#{user.name.capitalize} spent: $#{spent_value}"
+        remaining_message = "#{user.name.capitalize} remaining budget: $#{remaining_value}"
+
+        reply_message << spent_message
+        reply_message << remaining_message
+      end
+      send_message(@to_user, reply_message.join("\n- "))
+    end
 
     def get_status(user)
       user_budget_column = "#{user.key}_budget" # column name to query
@@ -71,7 +106,8 @@ module Commands
       user_spent = sheet[user_spent_column]
       user_budget = sheet[user_budget_column]
 
-      reply = "Remaining budget for **#{user.name.capitalize}** this month: $#{user_budget}. Total spent this month: $#{user_spent}."
+      reply = "Remaining budget for **#{user.name.capitalize}** this " /
+        "month: $#{user_budget}. Total spent this month: $#{user_spent}."
       send_message(@to_user, reply)
     end
 
